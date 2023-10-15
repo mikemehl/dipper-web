180bd2ff49dc75d4d1b46bf12ffffd597d908332
Mike Mehl <michaelalanmehl@gmail.com>
2023-10-15 14:11:15 -0400

    Audio playing working, yay.

--- app.lua
+++ app.lua
@@ -7,36 +7,36 @@ local Podcasts = Model:extend("podcasts")
 local Episodes = Model:extend("episodes")
 
 app:get("/", function()
-	return { render = "index" }
+  return { render = "index" }
 end)
 
 app:get("subscriptions", "/subscriptions(/:sort)", function(self)
-	self.sort = self.params.sort or "title"
-	if self.sort == "title" then
-		self.subs = Podcasts:select("ORDER BY title", nil, { fields = "title, id" })
-	else
-		self.subs = Podcasts:select(nil, nil, { fields = "title, id" })
-	end
-	return { render = "subscriptions" }
+  self.sort = self.params.sort or "title"
+  if self.sort == "title" then
+    self.subs = Podcasts:select("ORDER BY title", nil, { fields = "title, id" })
+  else
+    self.subs = Podcasts:select(nil, nil, { fields = "title, id" })
+  end
+  return { render = "subscriptions" }
 end)
 
 app:get("subscription", "/subscription/:id(/:id_or_episodes)", function(self)
-	if self.params.id_or_episodes == "episodes" then
-		print("HELLO")
-		self.episodes = Episodes:select("WHERE podcast_id = ?", self.params.id)
-		return { render = "pod_episodes" }
-	elseif type(self.params.id) == "number" then
-		self.episode = Episodes:find(self.params.id)
-		return { render = "episode" }
-	else
-		self.pod = Podcasts:find(self.params.id)
-		return { render = "subscription" }
-	end
+  if self.params.id_or_episodes == "episodes" then
+    print("HELLO")
+    self.episodes = Episodes:select("WHERE podcast_id = ?", self.params.id)
+    return { render = "pod_episodes" }
+  elseif type(self.params.id) == "number" then
+    self.episode = Episodes:find(self.params.id)
+    return { render = "episode" }
+  else
+    self.pod = Podcasts:find(self.params.id)
+    return { render = "subscription" }
+  end
 end)
 
 app:get("audio", "/audio/:id", function(self)
-	self.episode = Episodes:find(self.params.id)
-	return { render = "audio" }
+  self.episode = Episodes:find(self.params.id)
+  return { render = "audio" }
 end)
 
 return app
